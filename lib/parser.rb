24040a815d9fb53a3e143517fb82f0d0768e0f01
apoc64 <sschwedt@gmail.com>
2018-03-31 20:05:58 -0600

    turns request to a hash

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,11 +1,16 @@
+require 'pry'
+
 class Parser
 
-  def response(request)
-    assemble_response_string
+  def response(request_lines)
+    request = make_request_hash(request_lines)
+    puts request
+    html_body = "<h1>Hello World!</h1>"
+    assemble_response_string(html_body)
   end
 
-  def assemble_response_string
-    output = "<html><head></head><body><h1>Hello World!</h1></body></html>"
+  def assemble_response_string(html_body)
+    output = "<html><head></head><body>#{html_body}</body></html>"
     headers = ["http/1.1 200 ok",
       "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
       "server: ruby",
@@ -14,11 +19,28 @@ class Parser
     headers + output
   end
 
-
   #make hash
+  def make_request_hash(request_lines)
+    # binding.pry
+    request = {}
+    first_line = request_lines[0].split
+    request["Verb"] = first_line[0]
+    request["Path"] = first_line[1]
+    request["Protocol"] = first_line[2]
+    request_lines.each do |line|
+      # binding,pry
+      split = line.split(": ")
+      request[split[0]] = split[1]
+    end
+    # binding.pry
+    request
+  end
 
   #counter for hellos
-  
+  def hello_counter
+
+  end
+
   #counter for total requests
   #evaluate path
   #return value for shutdown
--- test/parser_test.rb
+++ test/parser_test.rb
@@ -24,20 +24,27 @@ class ParserTest < Minitest::Test
   end
 
   def test_it_can_assemble_response_string
-    response = @p.assemble_response_string
+    response = @p.assemble_response_string("<h1>Hello World!</h1>")
     assert_instance_of String, response
     assert_equal "http/1.1 200 ok", response[0..14]
-    assert response.length > 175
+    assert_equal response.length, 197
     assert response.include?("charset=iso-8859-1")
   end
 
   def test_it_can_count
+    skip
     assert_equal "0", @p.response("")[-20]
     @p.hello_counter
     assert_equal "1", @p.response("")[-20]
     @p.hello_counter
     @p.hello_counter
     assert_equal "3", @p.response("")[-20]
+  end
+
+  def test_it_makes_a_hash
+    request_lines = ["l: x", "z: r"]
+    request = @p.make_request_hash(request_lines)
+    assert_instance_of Hash, request
   end
 
 end
