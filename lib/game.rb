8668d1df1f45f03aa1d7a692a25dd9ee7dab6bb1
apoc64 <sschwedt@gmail.com>
2018-04-02 19:08:09 -0600

    game can evaluate answers

--- lib/game.rb
+++ lib/game.rb
@@ -3,14 +3,23 @@ class Game
 
   def initialize(answer = rand(100))
     @answer = answer
+    @guesses = []
   end
 
   def guess(number)
-
+    @guesses << number
   end
 
   def evaluate_guess
-
-  end 
+    return "You have to guess first" if @guesses.count == 0
+    guesses = @guesses.count > 1 ? "#{@guesses.count} guesses" : "1 guess"
+    if @guesses[-1] == @answer
+      "Your guess of #{@guesses[-1]} was correct. It took you #{guesses}"
+    elsif @guesses[-1] > @answer
+      "You have made #{guesses}. Your guess of #{@guesses[-1]} was too high"
+    else
+      "You have made #{guesses}. Your guess of #{@guesses[-1]} was too low"
+    end
+  end
 
 end
