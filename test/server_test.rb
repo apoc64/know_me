27f13bf378605592a996a6ebbdd44d420039b4bb
apoc64 <sschwedt@gmail.com>
2018-04-04 20:08:19 -0600

    passed a faraday test

--- test/server_test.rb
+++ test/server_test.rb
@@ -1,19 +1,18 @@
-# require 'minitest/autorun'
-# require 'minitest/pride'
-# require 'faraday'
-# require './lib/server'
-# require 'pry'
-# class ServerTest < Minitest::Test
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'faraday'
+require './lib/server'
+require 'pry'
+class ServerTest < Minitest::Test
 #
 #   # def test_it_exists
 #   # assert_instance_of Server,
 #   # end
 #
-#   def test_it_responds_to_request
-#     skip
-#     response = Faraday.get 'http://localhost:9292'
-#     expected = ""
-#     assert_equal expected, response
-#   end
+  def test_it_responds_to_request
+    # skip
+    response = Faraday.get 'http://localhost:9292'
+    assert_equal response.status, 200
+  end
 #
-# end
+end
